12e186ceaea8fd36f37b86f859b4959806b1b3f5
mohit4ever <mohit.kumar83549@gmail.com>
2024-10-18 18:06:25 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,326 @@
+[
+{
+    "_id": {
+      "$oid": "666ed2ec4145cdabc0ef9978"
+    },
+    "id": 5,
+    "name": "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    "image": "http://localhost:4000/images/product_1718538988401.png",
+    "category": "women",
+    "new_price": 60,
+    "old_price": 80,
+    "avilable": true,
+    "date": {
+      "$date": "2024-06-16T11:56:28.415Z"
+    },
+    "__v": 0
+  },
+
+  {
+    "_id": {
+      "$oid": "666ed3014145cdabc0ef997b"
+    },
+    "id": 6,
+    "name": "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    "image": "http://localhost:4000/images/product_1718539009349.png",
+    "category": "women",
+    "new_price": 60,
+    "old_price": 80,
+    "avilable": true,
+    "date": {
+      "$date": "2024-06-16T11:56:49.364Z"
+    },
+    "__v": 0
+  },
+  {
+    "_id": {
+      "$oid": "666ed30a4145cdabc0ef997e"
+    },
+    "id": 7,
+    "name": "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    "image": "http://localhost:4000/images/product_1718539018965.png",
+    "category": "women",
+    "new_price": 60,
+    "old_price": 80,
+    "avilable": true,
+    "date": {
+      "$date": "2024-06-16T11:56:58.977Z"
+    },
+    "__v": 0
+  },
+  {
+    "_id": {
+      "$oid": "666ed3194145cdabc0ef9981"
+    },
+    "id": 8,
+    "name": "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    "image": "http://localhost:4000/images/product_1718539033728.png",
+    "category": "women",
+    "new_price": 60,
+    "old_price": 80,
+    "avilable": true,
+    "date": {
+      "$date": "2024-06-16T11:57:13.741Z"
+    },
+    "__v": 0
+  },
+  {
+    "_id": {
+      "$oid": "666ed3234145cdabc0ef9984"
+    },
+    "id": 9,
+    "name": "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    "image": "http://localhost:4000/images/product_1718539043274.png",
+    "category": "women",
+    "new_price": 60,
+    "old_price": 80,
+    "avilable": true,
+    "date": {
+      "$date": "2024-06-16T11:57:23.289Z"
+    },
+    "__v": 0
+  },
+  {
+    "_id": {
+      "$oid": "666ed32d4145cdabc0ef9987"
+    },
+    "id": 10,
+    "name": "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    "image": "http://localhost:4000/images/product_1718539053705.png",
+    "category": "women",
+    "new_price": 60,
+    "old_price": 80,
+    "avilable": true,
+    "date": {
+      "$date": "2024-06-16T11:57:33.719Z"
+    },
+    "__v": 0
+  },
+  {
+    "_id": {
+      "$oid": "666ed3394145cdabc0ef998a"
+    },
+    "id": 11,
+    "name": "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    "image": "http://localhost:4000/images/product_1718539065417.png",
+    "category": "women",
+    "new_price": 60,
+    "old_price": 80,
+    "avilable": true,
+    "date": {
+      "$date": "2024-06-16T11:57:45.430Z"
+    },
+    "__v": 0
+  },
+  {
+    "_id": {
+      "$oid": "666ed3434145cdabc0ef998d"
+    },
+    "id": 12,
+    "name": "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    "image": "http://localhost:4000/images/product_1718539075816.png",
+    "category": "women",
+    "new_price": 60,
+    "old_price": 80,
+    "avilable": true,
+    "date": {
+      "$date": "2024-06-16T11:57:55.827Z"
+    },
+    "__v": 0
+  },
+  {
+    "_id": {
+      "$oid": "666ed3504145cdabc0ef9990"
+    },
+    "id": 13,
+    "name": "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    "image": "http://localhost:4000/images/product_1718539088060.png",
+    "category": "women",
+    "new_price": 60,
+    "old_price": 80,
+    "avilable": true,
+    "date": {
+      "$date": "2024-06-16T11:58:08.074Z"
+    },
+    "__v": 0
+  },
+  {
+    "_id": {
+      "$oid": "666ed3584145cdabc0ef9993"
+    },
+    "id": 14,
+    "name": "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    "image": "http://localhost:4000/images/product_1718539096200.png",
+    "category": "women",
+    "new_price": 60,
+    "old_price": 80,
+    "avilable": true,
+    "date": {
+      "$date": "2024-06-16T11:58:16.214Z"
+    },
+    "__v": 0
+  },
+
+  {
+    "_id": {
+      "$oid": "666ed3814145cdabc0ef9996"
+    },
+    "id": 15,
+    "name": "en Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    "image": "http://localhost:4000/images/product_1718539137324.png",
+    "category": "men",
+    "new_price": 70,
+    "old_price": 90,
+    "avilable": true,
+    "date": {
+      "$date": "2024-06-16T11:58:57.336Z"
+    },
+    "__v": 0
+  },
+
+  {
+    "_id": {
+      "$oid": "666ed38a4145cdabc0ef9999"
+    },
+    "id": 16,
+    "name": "en Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    "image": "http://localhost:4000/images/product_1718539146786.png",
+    "category": "men",
+    "new_price": 70,
+    "old_price": 90,
+    "avilable": true,
+    "date": {
+      "$date": "2024-06-16T11:59:06.802Z"
+    },
+    "__v": 0
+  },
+  {
+    "_id": {
+      "$oid": "666ed39e4145cdabc0ef999c"
+    },
+    "id": 17,
+    "name": "en Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    "image": "http://localhost:4000/images/product_1718539166762.png",
+    "category": "men",
+    "new_price": 70,
+    "old_price": 90,
+    "avilable": true,
+    "date": {
+      "$date": "2024-06-16T11:59:26.778Z"
+    },
+    "__v": 0
+  },
+  {
+    "_id": {
+      "$oid": "666ed3a84145cdabc0ef999f"
+    },
+    "id": 18,
+    "name": "en Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    "image": "http://localhost:4000/images/product_1718539176490.png",
+    "category": "men",
+    "new_price": 70,
+    "old_price": 90,
+    "avilable": true,
+    "date": {
+      "$date": "2024-06-16T11:59:36.502Z"
+    },
+    "__v": 0
+  },
+  {
+    "_id": {
+      "$oid": "666ed3b44145cdabc0ef99a2"
+    },
+    "id": 19,
+    "name": "en Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    "image": "http://localhost:4000/images/product_1718539188321.png",
+    "category": "men",
+    "new_price": 70,
+    "old_price": 90,
+    "avilable": true,
+    "date": {
+      "$date": "2024-06-16T11:59:48.335Z"
+    },
+    "__v": 0
+  },
+  {
+    "_id": {
+      "$oid": "666ed3bc4145cdabc0ef99a5"
+    },
+    "id": 20,
+    "name": "en Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    "image": "http://localhost:4000/images/product_1718539196976.png",
+    "category": "men",
+    "new_price": 70,
+    "old_price": 90,
+    "avilable": true,
+    "date": {
+      "$date": "2024-06-16T11:59:56.988Z"
+    },
+    "__v": 0
+  },
+  {
+    "_id": {
+      "$oid": "666ed3c44145cdabc0ef99a8"
+    },
+    "id": 21,
+    "name": "en Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    "image": "http://localhost:4000/images/product_1718539204294.png",
+    "category": "men",
+    "new_price": 70,
+    "old_price": 90,
+    "avilable": true,
+    "date": {
+      "$date": "2024-06-16T12:00:04.305Z"
+    },
+    "__v": 0
+  },
+  {
+    "_id": {
+      "$oid": "666ed3cd4145cdabc0ef99ab"
+    },
+    "id": 22,
+    "name": "en Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    "image": "http://localhost:4000/images/product_1718539213526.png",
+    "category": "men",
+    "new_price": 70,
+    "old_price": 90,
+    "avilable": true,
+    "date": {
+      "$date": "2024-06-16T12:00:13.539Z"
+    },
+    "__v": 0
+  },
+  {
+    "_id": {
+      "$oid": "666ed3fc4145cdabc0ef99ae"
+    },
+    "id": 23,
+    "name": "Boys Orange Colour blocked Hooded Sweatshirt",
+    "image": "http://localhost:4000/images/product_1718539260718.png",
+    "category": "kid",
+    "new_price": 50,
+    "old_price": 80,
+    "avilable": true,
+    "date": {
+      "$date": "2024-06-16T12:01:00.729Z"
+    },
+    "__v": 0
+  },
+  {
+    "_id": {
+      "$oid": "666ed4034145cdabc0ef99b1"
+    },
+    "id": 24,
+    "name": "Boys Orange Colour blocked Hooded Sweatshirt",
+    "image": "http://localhost:4000/images/product_1718539267912.png",
+    "category": "kid",
+    "new_price": 50,
+    "old_price": 80,
+    "avilable": true,
+    "date": {
+      "$date": "2024-06-16T12:01:07.925Z"
+    },
+    "__v": 0
+  }
+
+]
